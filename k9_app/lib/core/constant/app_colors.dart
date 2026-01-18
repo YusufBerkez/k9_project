f9af7bb0bada55b912dc967ac687f4e333c23042
YusufBerkez <yusufberke1906@gmail.com>
2026-01-18 18:39:12 +0300

    Bottomnavbar rengi düzeltildi

--- k9_app/lib/core/constant/app_colors.dart
+++ k9_app/lib/core/constant/app_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color darkModeCardBorder= Color(0xFF616161);
-  static const Color lightModeCardBorder=Color(0xFFEEEEEE);
-  static const Color darkModeTextColor=Color(0xFFEEEEEE);
-  static const Color lightModeTextColor=Color(0xFF616161);
-  static const Color darkModeBottomNavbar= Color(0x001B1B1F);
+  static const Color darkModeCardBorder = Color(0xFF616161);
+  static const Color lightModeCardBorder = Color(0xFFEEEEEE);
+  static const Color darkModeTextColor = Color(0xFFEEEEEE);
+  static const Color lightModeTextColor = Color(0xFF616161);
+  static const Color darkModeBottomNavbar = Color(0x001B1B1F);
 }
